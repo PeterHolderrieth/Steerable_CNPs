c3fc3deb35b8595597f4118731f2115501bb14a4
PeterHolderrieth <peter.holderrieth@new.ox.ac.uk>
2020-07-25 18:00:20 +0100

    Rewrite tasks

---
+++ Tasks/GP_div_free/gp_sampler.py
@@ -0,0 +1,28 @@
+#LIBRARIES:
+#Tensors:
+import torch
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data as utils
+
+#E(2)-steerable CNNs - librar"y:
+from e2cnn import gspaces                                          
+from e2cnn import nn as G_CNN   
+#import e2cnn
+
+#Plotting in 2d/3d:
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.patches import Ellipse
+from matplotlib.colors import Normalize
+import matplotlib.cm as cm
+
+#Tools:
+import datetime
+import sys
+sys.path.append("../..")
+
+#Own files:
+import Kernel_and_GP_tools as GP
+import My_Tools
