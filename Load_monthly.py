1a85685743a69d296000e4408edbeb7c6e7c839a
PeterHolderrieth <peter.holderrieth@new.ox.ac.uk>
2020-07-31 23:55:02 +0200

    Wrote Climate data loader

--- Load_monthly.py
+++ Load_monthly.py
@@ -1,44 +1,45 @@
 import cdsapi
 
 c = cdsapi.Client()
+variables=['wind_speed','wind_direction','temperature','relative_humidity','pressure']
 
-for i in range(2019,1960):
-        year=str(i)
-        for j in range(1,13):
-                month=str(j)
-                if j<10:
-                        month='0'+month
-                
-                filename='UERRA'+year+'_'+month+'.grib'
-                print(filename)
-                '''        
-                c.retrieve(
-                'reanalysis-uerra-europe-height-levels',
-                {
-                        'variable': 'wind_speed',
-                        'height_level': [
-                        '15_m', '500_m',
-                        ],
-                        'year': '1961',
-                        'month': '02',
-                        'day': [
-                        '01', '02', '03',
-                        '04', '05', '06',
-                        '07', '08', '09',
-                        '10', '11', '12',
-                        '13', '14', '15',
-                        '16', '17', '18',
-                        '19', '20', '21',
-                        '22', '23', '24',
-                        '25', '26', '27',
-                        '28',
-                        ],
-                        'time': [
-                        '00:00', '06:00', '12:00',
-                        '18:00',
-                        ],
-                        'format': 'grib',
-                },
-                '.grib')
-                '''
-                
+for var in variables:
+        for i in range(2019,1960,-1):
+                year=str(i)
+                for j in range(12,0,-1):
+                        month=str(j)
+                        if j<10:
+                                month='0'+month
+                        
+                        filename='UERRA_'+var+'_'+year+'_'+month+'.grib'
+                              
+                        c.retrieve(
+                        'reanalysis-uerra-europe-height-levels',
+                        {
+                                'variable': var,
+                                'height_level': [
+                                '15_m', '500_m',
+                                ],
+                                'year': year,
+                                'month': month,
+                                'day': [
+                                '01', '02', '03',
+                                '04', '05', '06',
+                                '07', '08', '09',
+                                '10', '11', '12',
+                                '13', '14', '15',
+                                '16', '17', '18',
+                                '19', '20', '21',
+                                '22', '23', '24',
+                                '25', '26', '27',
+                                '28',
+                                ],
+                                'time': [
+                                '00:00', '06:00', '12:00',
+                                '18:00',
+                                ],
+                                'format': 'grib',
+                        },
+                        filename)
+                        
+                        
