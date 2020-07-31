06e4cc6eb62a798728923409bdefc11003a05a60
PeterHolderrieth <peter.holderrieth@new.ox.ac.uk>
2020-07-31 23:49:05 +0200

    Login update

--- Load_monthly.py
+++ Load_monthly.py
@@ -2,31 +2,43 @@ import cdsapi
 
 c = cdsapi.Client()
 
-c.retrieve(
-            'reanalysis-uerra-europe-height-levels',
+for i in range(2019,1960):
+        year=str(i)
+        for j in range(1,13):
+                month=str(j)
+                if j<10:
+                        month='0'+month
+                
+                filename='UERRA'+year+'_'+month+'.grib'
+                print(filename)
+                '''        
+                c.retrieve(
+                'reanalysis-uerra-europe-height-levels',
                 {
-                            'variable': 'wind_speed',
-                                    'height_level': [
-                                                    '15_m', '500_m',
-                                                            ],
-                                            'year': '2019',
-                                                    'month': '02',
-                                                            'day': [
-                                                                            '01', '02', '03',
-                                                                                        '04', '05', '06',
-                                                                                                    '07', '08', '09',
-                                                                                                                '10', '11', '12',
-                                                                                                                            '13', '14', '15',
-                                                                                                                                        '16', '17', '18',
-                                                                                                                                                    '19', '20', '21',
-                                                                                                                                                                '22', '23', '24',
-                                                                                                                                                                            '25', '26', '27',
-                                                                                                                                                                                        '28',
-                                                                                                                                                                                                ],
-                                                                    'time': [
-                                                                                    '00:00', '06:00', '12:00',
-                                                                                                '18:00',
-                                                                                                        ],
-                                                                            'format': 'grib',
-                                                                                },
-                    'download.grib')
+                        'variable': 'wind_speed',
+                        'height_level': [
+                        '15_m', '500_m',
+                        ],
+                        'year': '1961',
+                        'month': '02',
+                        'day': [
+                        '01', '02', '03',
+                        '04', '05', '06',
+                        '07', '08', '09',
+                        '10', '11', '12',
+                        '13', '14', '15',
+                        '16', '17', '18',
+                        '19', '20', '21',
+                        '22', '23', '24',
+                        '25', '26', '27',
+                        '28',
+                        ],
+                        'time': [
+                        '00:00', '06:00', '12:00',
+                        '18:00',
+                        ],
+                        'format': 'grib',
+                },
+                '.grib')
+                '''
+                
