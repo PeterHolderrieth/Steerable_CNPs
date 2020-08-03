32754d19309581e001a16983c16f945d2fa62d55
PeterHolderrieth <peter.holderrieth@new.ox.ac.uk>
2020-08-03 11:40:06 +0200

    Logout update

--- Tasks/ERA5_Cfa/Cfa_US/Format_CSV_Cfa_US_Hourly.py
+++ Tasks/ERA5_Cfa/Cfa_US/Format_CSV_Cfa_US_Hourly.py
@@ -1,6 +1,6 @@
 import pandas as pd
-old_filename="Data/ERA5_Cfa_US_unformated.csv"
-new_filename="Data/ERA5_Cfa_US.csv"
+old_filename="Data/By_hour/ERA5_Cfa_US_time_00_unformatted.csv"
+new_filename="Data/By_hour/ERA5_Cfa_US_time_00.csv"
 #Split the longitude , latitude and the value in different columns:
 with open(old_filename, 'r') as f_in, open(new_filename, 'w') as f_out:
     head=next(f_in).replace(" ","")
