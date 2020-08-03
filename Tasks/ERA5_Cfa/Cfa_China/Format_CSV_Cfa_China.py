560029ea46649968174e845b55bae8027b7b9d14
PeterHolderrieth <peter.holderrieth@new.ox.ac.uk>
2020-08-03 09:13:15 +0200

    Login update

---
+++ Tasks/ERA5_Cfa/Cfa_China/Format_CSV_Cfa_China.py
@@ -0,0 +1,8 @@
+import pandas as pd
+old_filename="Data/ERA5_Cfa_China_unformatted.csv"
+new_filename="Data/ERA5_Cfa_China.csv"
+#Split the longitude , latitude and the value in different columns:
+with open(old_filename, 'r') as f_in, open(new_filename, 'w') as f_out:
+    head=next(f_in).replace(" ","")
+    f_out.write(head)
+    [f_out.write(','.join(line.split()) + '\n') for line in f_in if line[:8]!="Latitude"]
