6ecd983006742445a20b18350c8af47a3aab5f5d
PeterHolderrieth <peter.holderrieth@new.ox.ac.uk>
2021-02-01 08:14:16 +0100

    Update

--- Experiments/ERA5/Run_GP_Grid_Search_2.sh
+++ Experiments/ERA5/Run_GP_Grid_Search_2.sh
@@ -1,12 +1,12 @@
 #Best so far: {'l_scale': 10.0, 'sigma_var': 0.1, 'kernel_type': 'rbf', 'obs_noise': 0.01}
 
-for lscale in 5. 7. 9. 10. 13. 15.
+for lscale in 13. 15.
 do
     for sigma in 0.05 0.1 0.25 0.5 1.
     do
         for noise in 0.005 0.01 0.02 0.05 0.1 
         do 
-             python Run_GP.py -data big -lscale $lscale -sigma $sigma -noise $noise
+             python Run_GP.py -data small -lscale $lscale -sigma $sigma -noise $noise
         done
     done
 done
